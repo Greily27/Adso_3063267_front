00aa41508be84e9899c4be75b2f83c89a279445b
greily27 <camargogreily07@gmail.com>
2026-06-24 13:24:13 -0500

    RutaCorreoElectronico_greily240626

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -20,6 +20,10 @@ export const AUTH_ROUTES: Routes = [
         path: 'reset-password/:token',
         loadComponent: () => import('./reset-password/reset-password').then(m => m.ResetPassword)
       },
+      {
+        path: 'reset-password',
+        loadComponent: () => import('./reset-password/reset-password').then(m => m.ResetPassword)
+      },
       { path: '', redirectTo: 'login', pathMatch: 'full' }
     ]
   }
